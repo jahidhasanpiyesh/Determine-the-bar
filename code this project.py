b56e33a598570467e7a142cabb7ad2291c1254f3
Md. Jahid Hasan <122719439+jahidhasanpiyesh@users.noreply.github.com>
2023-08-26 02:11:03 +0600

    Add files via upload

---
+++ code this project.py
@@ -0,0 +1,30 @@
+mas = str(input("Enter the 12 any Mounts name:"))
+date = int(input("Enter the date:"))
+for i in range(1, 31, 7):
+    if mas == "January":
+        if i == date:
+            print("Sunday")
+for i in range(2, 31, 7):
+    if mas == "January":
+        if i == date:
+            print("Monday")
+for i in range(3, 32, 7):
+    if mas == "January":
+        if i == date:
+            print("Tuesday")
+for i in range(4, 31, 7):
+    if mas == "January":
+        if i == date:
+            print("Wednesday")
+for i in range(5, 31, 7):
+    if mas == "January":
+        if i == date:
+            print("Thursday")
+for i in range(6, 31, 7):
+    if mas == "January":
+        if i == date:
+            print("Friday")
+for i in range(7, 31, 7):
+    if mas == "January":
+        if i == date:
+            print("Saturday")
